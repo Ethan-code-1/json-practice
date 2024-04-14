8e6c230e5332c45fe2661ffbe135a33c040cf54a
Ethan-code-1 O <ebo4dq@virginia.edu>
2024-04-14 20:32:47 +0000

    Finished first script utalizing array slicing and indexing

---
+++ submission/lab8/jq_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+curl -s "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" | jq -r .[:6][].receiptTime
+#Opted for more concise indexing method and added silent flag to hide curl feedback
+
+
